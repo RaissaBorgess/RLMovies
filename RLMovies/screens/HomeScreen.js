3c404cd7d559c65663ef97d8b21eac03bb50f606
RaissaBorgess <raissasantos123.rs33@gmail.com>
2024-11-21 11:39:05 -0300

    começar na 1322

--- RLMovies/screens/HomeScreen.js
+++ RLMovies/screens/HomeScreen.js
@@ -1,44 +1,57 @@
 import React, { useState } from 'react'
 import { View, Text, Platform, TouchableOpacity, ScrollView } from 'react-native'
-import { StatusBar } from 'react-native'
-import { Bars3BottomLeftIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
-import React from 'react'
-import { styles } from '../theme'
+import { StatusBar } from 'expo-status-bar'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import TrendingMovies from '../components/trendingMovies'
-import MovieList from '../components/movieList'
+import { Bars3BottomLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import { styles } from '../theme';
+import { useNavigation } from '@react-navigation/native'
+import TrendingMovies from '../components/trendingMovies';
+import MovieList from '../components/movieList';
+import Loading from '../components/loading';
 
-const ios = Platform.OS == 'ios';
+const ios = Platform.OS == "ios";
 
 export default function HomeScreen() {
-  const [trending, setTrending] = useState([1,2,3])
-  const [upcoming, setUpcoming] = useState([1,2,3,4])
-  const [topRated, setTopRated] = useState([1,2,3,4,5])
+  const [trending, setTrending] = useState([1, 2, 3]);
+  const [upcoming, setUpcoming] = useState([1, 2, 3]);
+  const [topRated, setTopRated] = useState([1, 2, 3]);
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
+
   return (
     <View className="flex-1 bg-neutral-800">
-      <SafeAreaView className={ ios ? '-mb2' : 'mb-3'}>
-        <StatusBar style='light'/>
+      <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
+        <StatusBar style="light" />
         <View className="flex-row justify-between items-center mx-4 mt-2">
           <TouchableOpacity>
-            <Bars3BottomLeftIcon size={30} strokeWidth={2} color='white'/>
+            <Bars3BottomLeftIcon size={30} strokeWidth={2} color="white" />
           </TouchableOpacity>
-          <Text className='text-white text-3xl font-bold'>
-          <Text style={styles.text}>RL</Text>Movies
+          <Text className="text-white text-3xl font-bold">
+            <Text style={styles.text}>GM</Text>ovies
           </Text>
-          <TouchableOpacity>
-          <Bars3BottomLeftIcon size={30} strokeWidth={2} color='white'/>
+          <TouchableOpacity onPress={() => navigation.navigate('Search')}>
+            <MagnifyingGlassIcon size={30} strokeWidth={2} color="white" />
           </TouchableOpacity>
         </View>
       </SafeAreaView>
-      
-      <ScrollView
-      showVerticalScrollIndicator={false}
-      contentContainerStyles={{ paddingBottom: 10 }}
-      >
-        <TrendingMovies data={trending} />
-        <MovieList title="Próximos Lançamentos" data={upcoming} />
-        <MovieList title="Melhores Avaliados" data={topRated} />
-      </ScrollView>
+
+      {
+        loading ? (
+          <Loading />
+        ) : (
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 10 }}
+          >
+            <TrendingMovies data={trending} />
+
+            <MovieList title="Próximos lançamentos" data={upcoming} />
+
+            <MovieList title="Melhor Avaliados" data={topRated} />
+          </ScrollView>
+        )
+      }
+
     </View>
   )
 }
